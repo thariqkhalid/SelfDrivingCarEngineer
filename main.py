23970fa89b98536c37a0328ee159c724b71c3596
THARIQ KHALID <thariq.khalid@gmail.com>
2020-12-29 14:42:12 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+
+def
+    image = mpimg.imread('test.jpg')
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
